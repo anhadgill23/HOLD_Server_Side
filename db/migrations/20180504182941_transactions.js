02a1f8f3a56729abcc683345ac069dc81da6c1df
Andy <voandy1998@gmail.com>
2018-05-04 20:09:04 +0000

    created users and transactions tables

---
+++ db/migrations/20180504182941_transactions.js
@@ -0,0 +1,18 @@
+exports.up = function create( knex, Promise ) {
+  return Promise.all( [
+    knex.schema.createTable( 'transactions', ( table ) => {
+      table.increments();
+      table.date( 'date' );
+      table.string( 'coin' );
+      table.float( 'cost' );
+      table.integer( 'amount' );
+      table.string( 'type' );
+      table.integer( 'user_id' ).references( 'id' ).inTable( 'users' );
+    } ),
+  ] );
+};
+exports.down = function drop( knex, Promise ) {
+  return Promise.all( [
+    knex.schema.dropTable( 'transactions' ),
+  ] );
+};
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require( 'express' );
 
+// const ENV = process.env.ENV || 'development';
+
 const app = express();
 
 const PORT = process.env.PORT || 8080; // default port 8080
+
+// const knexConfig = require( './knexfile' );
+// const knex = require( 'knex' )( knexConfig[ENV] );
+
+// const knexLogger = require( 'knex-logger' );
+
+// const pollRoutes = require( './routes/polls' );
 
 app.get( '/api/urls', ( req, res ) => {
   res.send( {
