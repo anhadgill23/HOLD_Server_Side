133e645ea06fe8262332224715552a5f687546ee
Andy <voandy1998@gmail.com>
2018-05-05 19:38:56 +0000

    created http request for currentPrice

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 8080; // default port 8080
 
 const knexConfig = require( './knexfile' );
 const knex = require( 'knex' )( knexConfig[ENV] );
+const request = require( 'request' );
 
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
@@ -19,32 +20,52 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // Selects all symbols a user has purchased
 // query all user transactions manually calculate profit/loss swell as total coin holdings create and send in json res.json
-app.get( '/users_id', ( req, res ) => {
+app.get( '/:users_id', ( req, res ) => {
   knex.select().from( 'transactions' )
-    .where( { users_id: 2 } )
+    .where( { users_id: req.params.users_id } )
     .then( ( result ) => {
       res.send( result );
     } );
 } );
 
 // Selects all transactions of a user of a certain symbol
-app.get( '/users_id/symbol', ( req, res ) => {
-  knex.select().from( 'transactions' ).where( { symbol: 'BTC', users_id: 2 } )
+app.get( '/:users_id/:symbol', ( req, res ) => {
+  knex.select().from( 'transactions' ).where( { symbol: req.params.symbol, users_id: req.params.users_id } )
     .then( ( result ) => {
       res.send( result );
     } );
 } );
 
+// { id: 2, symbol: 'BTC', price: 10.8, amount: 1, users_id: 2 }
 // Selects a specific transaction
-app.get( '/transactions/transaction_id', ( req, res ) => {
-  knex.select().from( 'transactions' ).where( { id: 1 } )
+app.get( '/transactions/:transaction_id', ( req, res ) => {
+  const transaction_id = req.params.transaction_id;
+  knex.select().from( 'transactions' ).where( { id: transaction_id } )
+    .then( result => result )
     .then( ( result ) => {
-      console.log( result[0].symbol );
+      const transaction = result[0];
+      const symbol = transaction.symbol;
+      const buyPrice = transaction.price;
+      const tradingPair = `${symbol}/USD`;
+      const amount = transaction.amount;
+      const transactionCost = amount * buyPrice;
+      const userTransaction = {
+        symbol, buyPrice, tradingPair, amount, transactionCost,
+      };
+      console.log( userTransaction );
+      request( `https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD`, ( error, response, body ) => {
+        const currentPrice = JSON.parse( body ).USD;
+        const currentWorth = currentPrice * amount;
+        const profit = ( currentWorth - transactionCost ) / transactionCost;
+        userTransaction.currentWorth = currentWorth;
+        userTransaction.profit = profit;
+        res.json( userTransaction );
+      } );
     } );
 } );
 
 // use postman to send json
-app.post( '/transactions/users_id', ( req, res ) => {
+app.post( '/transactions/:users_id', ( req, res ) => {
   // knex.insert( req.body ).into( 'transactions' );
   res.json( req.body );
 } );
